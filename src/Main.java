ad2838855f2b75bbfc86ccfcc230485684f1f957
avihay Abramov <avihay@avihays-Air.lan>
2025-05-02 05:07:42 -0700

    MyDinamicSet mission

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
+
 public class Main {
     public static void main(String[] args) {
-        MyAVLTree<String> tree = new MyAVLTree<>();
+        MyDynamicSet<String> set = new MyDynamicSet<>(10);
 
-        tree.insert(new TreeNode<>(10, "A"));
-        tree.insert(new TreeNode<>(5, "B"));
-        tree.insert(new TreeNode<>(3, "C"));
-        tree.insert(new TreeNode<>(7, "D"));
-        tree.insert(new TreeNode<>(1, "E"));
+        // הכנסת איברים (לא שומרים את המצביעים!)
+        set.insert(new Element<>(5, "five"));
+        set.insert(new Element<>(2, "two"));
+        set.insert(new Element<>(8, "eight"));
 
-        System.out.println("Depth of min: " + tree.depthOfMin());
+        // חיפוש כדי לקבל את המצביעים הנכונים (ListLink<T>)
+        Element<String> five = set.search(5);
+        Element<String> two = set.search(2);
+        Element<String> eight = set.search(8);
 
+        // בדיקות
+        System.out.println("Search 5: " + five);         // [5]
+        System.out.println("Search 4: " + set.search(4)); // null
+
+        System.out.println("Minimum: " + set.minimum()); // [2]
+        System.out.println("Maximum: " + set.maximum()); // [8]
+
+        System.out.println("Successor of 5: " + set.successor(five));     // [8]
+        System.out.println("Predecessor of 5: " + set.predecessor(five)); // [2]
+
+        // מחיקה
+        set.delete(five);
+
+        // בדיקה מחדש אחרי מחיקה
+        System.out.println("After deleting 5:");
+        System.out.println("Search 5: " + set.search(5));           // null
+        System.out.println("Successor of 2: " + set.successor(two));   // [8]
+        System.out.println("Predecessor of 8: " + set.predecessor(eight)); // [2]
     }
 }
--- src/MyDynamicSet.java
+++ src/MyDynamicSet.java
@@ -2,10 +2,11 @@
  * @param <T> The type of the satellite data of the elements in the dynamic-set.
  */
 public class MyDynamicSet<T> {
+	private MySortedLinkedList<T> list;
 	/*
      * You may add any fields that you wish to add.
      * Remember that the use of built-in Java classes is not allowed,
-     * the only variables types you can use are: 
+     * the only variables types you can use are:
      * 	-	the given classes in the assignment
      * 	-	basic arrays
      * 	-	primitive variables
@@ -17,34 +18,35 @@ public class MyDynamicSet<T> {
 	 * @param N The maximum number of elements in the dynamic set at each time.
 	 */
 	public MyDynamicSet(int N) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		this.list = new MySortedLinkedList<>();
 	}
-	
+
 	public Element<T> search(int k) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return list.search(k);
 	}
-	
+
 	public void insert(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		ListLink newNode = new ListLink(x);
+		list.insert(newNode);
 	}
-	
+
 	public void delete(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		list.delete((ListLink<T>)x);
 	}
-	
+
 	public Element<T> minimum() {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return list.head();
 	}
-	
+
 	public Element<T> maximum() {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return list.tail();
 	}
-	
+
 	public Element<T> successor(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return ((ListLink<T>)x).getNext();
 	}
-	
+
 	public Element<T> predecessor(Element<T> x) {
-		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+		return ((ListLink<T>)x).getPrev();
 	}
 }
